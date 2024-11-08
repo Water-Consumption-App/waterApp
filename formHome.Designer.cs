e5155d4eb03d1c0cd62a6dfd1fe9126c9d45d5d9
Fernando Lopes <Fernando_maxi@outlook.com>
2024-11-08 03:10:37 -0300

    Feat: add new page home
    add a new expremental code for tests

--- formHome.Designer.cs
+++ formHome.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class formHome
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,29 +13,60 @@
             base.Dispose(disposing);
         }
 
-
-
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             label1 = new Label();
+            labelStatistics = new Label();
+            labelLegalInfo = new Label();
+            progressBarConsumption = new ProgressBar();
+
             SuspendLayout();
+
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(451, 324);
+            label1.Location = new Point(451, 50);
             label1.Name = "label1";
             label1.Size = new Size(127, 47);
             label1.TabIndex = 0;
             label1.Text = "HOME";
             label1.TextAlign = ContentAlignment.MiddleCenter;
+
+            // 
+            // labelStatistics
+            // 
+            labelStatistics.AutoSize = true;
+            labelStatistics.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelStatistics.Location = new Point(100, 150);
+            labelStatistics.Name = "labelStatistics";
+            labelStatistics.Size = new Size(400, 21);
+            labelStatistics.TabIndex = 1;
+            labelStatistics.Text = "Estatísticas: Consumo de água esta semana foi 20% menor.";
+
+            // 
+            // progressBarConsumption
+            // 
+            progressBarConsumption.Location = new Point(100, 200);
+            progressBarConsumption.Name = "progressBarConsumption";
+            progressBarConsumption.Size = new Size(400, 30);
+            progressBarConsumption.TabIndex = 2;
+            progressBarConsumption.Value = 50; // Exemplo: 50% do consumo atingido dps tem q add o valor real, nao ffaço ideia de api <3
+
+            // 
+            // labelLegalInfo
+            // 
+            labelLegalInfo.AutoSize = true;
+            labelLegalInfo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelLegalInfo.Location = new Point(100, 270);
+            labelLegalInfo.Name = "labelLegalInfo";
+            labelLegalInfo.Size = new Size(350, 21);
+            labelLegalInfo.TabIndex = 3;
+            labelLegalInfo.Text = "Aviso: Respeite as normas locais de consumo de água.";
+
             // 
             // formHome
             // 
@@ -50,6 +74,9 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1080, 720);
             Controls.Add(label1);
+            Controls.Add(labelStatistics);
+            Controls.Add(progressBarConsumption);
+            Controls.Add(labelLegalInfo);
             FormBorderStyle = FormBorderStyle.None;
             Name = "formHome";
             Text = "formHome";
@@ -61,5 +88,8 @@
         #endregion
 
         private Label label1;
+        private Label labelStatistics;
+        private Label labelLegalInfo;
+        private ProgressBar progressBarConsumption;
     }
 }
