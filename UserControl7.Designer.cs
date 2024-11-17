3b02a92747891bd0acc0bb394dc9dc937b56a24d
Fernando Lopes <Fernando_maxi@outlook.com>
2024-11-17 07:13:11 -0300

    fix: Fixed the UserControl7 page with a new look

--- UserControl7.Designer.cs
+++ UserControl7.Designer.cs
@@ -28,21 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ucPage1));
             textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.BackColor = Color.FromArgb(0, 14, 39);
-            textBox1.Font = new Font("Tahoma", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.BackColor = Color.FromArgb(10, 29, 55);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Tahoma", 30F, FontStyle.Bold, GraphicsUnit.Point, 0);
             textBox1.ForeColor = Color.White;
-            textBox1.Location = new Point(85, 293);
+            textBox1.Location = new Point(24, 100);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(910, 86);
+            textBox1.Size = new Size(1038, 551);
             textBox1.TabIndex = 0;
-            textBox1.Text = "Aqui será a apresentação do trabalho";
+            textBox1.Text = resources.GetString("textBox1.Text");
             textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(10, 29, 55);
+            label1.Font = new Font("Tahoma", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(246, 66);
+            label1.Name = "label1";
+            label1.Size = new Size(574, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Oláá seja muito bem vindo ao                                     xD";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(10, 29, 55);
+            label2.Font = new Font("Tahoma", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Yellow;
+            label2.Location = new Point(565, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(218, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Poseidon´s Journey";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(10, 29, 55);
+            pictureBox1.Location = new Point(10, 49);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1066, 615);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
             // 
             // ucPage1
             // 
@@ -50,9 +89,13 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(0, 14, 39);
             BackgroundImageLayout = ImageLayout.None;
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
             Name = "ucPage1";
             Size = new Size(1080, 720);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -60,5 +103,8 @@
         #endregion
 
         private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
     }
 }
