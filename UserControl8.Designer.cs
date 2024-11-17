3707616d0346078334f774be3f6a8029d087b9e3
Fernando Lopes <Fernando_maxi@outlook.com>
2024-11-17 06:59:19 -0300

    Fix: a new page UserControl8

--- UserControl8.Designer.cs
+++ UserControl8.Designer.cs
@@ -28,9 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ucPage2));
             label1 = new Label();
-            textBox1 = new TextBox();
+            btn_opc1_uc8 = new Button();
+            btn_opc2_uc8 = new Button();
+            btn_opc3_uc8 = new Button();
+            btn_opc4_uc8 = new Button();
+            btn_opc5_uc8 = new Button();
+            btn_opc6_uc8 = new Button();
+            btn_opc7_uc8 = new Button();
+            btn_opc8_uc8 = new Button();
+            btn_opc9_uc8 = new Button();
+            txt_opc1_uc8 = new TextBox();
+            txt_opc2_uc8 = new TextBox();
+            txt_opc3_uc8 = new TextBox();
+            txt_opc4_uc8 = new TextBox();
+            txt_opc5_uc8 = new TextBox();
+            txt_opc6_uc8 = new TextBox();
+            txt_opc7_uc8 = new TextBox();
+            txt_opc8_uc8 = new TextBox();
+            txt_opc9_uc8 = new TextBox();
             SuspendLayout();
             // 
             // label1
@@ -45,25 +61,303 @@
             label1.Text = "Você sabia que . . .";
             label1.Click += label1_Click;
             // 
-            // textBox1
+            // btn_opc1_uc8
             // 
-            textBox1.BackColor = Color.FromArgb(0, 14, 39);
-            textBox1.Font = new Font("Tahoma", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            textBox1.ForeColor = Color.White;
-            textBox1.Location = new Point(53, 149);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(1002, 552);
-            textBox1.TabIndex = 1;
-            textBox1.Text = resources.GetString("textBox1.Text");
-            textBox1.TextAlign = HorizontalAlignment.Center;
+            btn_opc1_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            btn_opc1_uc8.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_opc1_uc8.Cursor = Cursors.Hand;
+            btn_opc1_uc8.Font = new Font("Tahoma", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_opc1_uc8.ForeColor = Color.White;
+            btn_opc1_uc8.Location = new Point(168, 155);
+            btn_opc1_uc8.Margin = new Padding(3, 2, 3, 2);
+            btn_opc1_uc8.Name = "btn_opc1_uc8";
+            btn_opc1_uc8.Size = new Size(150, 150);
+            btn_opc1_uc8.TabIndex = 1;
+            btn_opc1_uc8.Text = "1";
+            btn_opc1_uc8.UseVisualStyleBackColor = false;
+            btn_opc1_uc8.Click += btn_opc1_uc8_Click;
+            // 
+            // btn_opc2_uc8
+            // 
+            btn_opc2_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            btn_opc2_uc8.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_opc2_uc8.Cursor = Cursors.Hand;
+            btn_opc2_uc8.Font = new Font("Tahoma", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_opc2_uc8.ForeColor = Color.White;
+            btn_opc2_uc8.Location = new Point(473, 155);
+            btn_opc2_uc8.Margin = new Padding(3, 2, 3, 2);
+            btn_opc2_uc8.Name = "btn_opc2_uc8";
+            btn_opc2_uc8.Size = new Size(150, 150);
+            btn_opc2_uc8.TabIndex = 1;
+            btn_opc2_uc8.Text = "2";
+            btn_opc2_uc8.UseVisualStyleBackColor = false;
+            btn_opc2_uc8.Click += btn_opc2_uc8_Click;
+            // 
+            // btn_opc3_uc8
+            // 
+            btn_opc3_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            btn_opc3_uc8.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_opc3_uc8.Cursor = Cursors.Hand;
+            btn_opc3_uc8.Font = new Font("Tahoma", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_opc3_uc8.ForeColor = Color.White;
+            btn_opc3_uc8.Location = new Point(776, 155);
+            btn_opc3_uc8.Margin = new Padding(3, 2, 3, 2);
+            btn_opc3_uc8.Name = "btn_opc3_uc8";
+            btn_opc3_uc8.Size = new Size(150, 150);
+            btn_opc3_uc8.TabIndex = 1;
+            btn_opc3_uc8.Text = "3";
+            btn_opc3_uc8.UseVisualStyleBackColor = false;
+            btn_opc3_uc8.Click += btn_opc3_uc8_Click;
+            // 
+            // btn_opc4_uc8
+            // 
+            btn_opc4_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            btn_opc4_uc8.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_opc4_uc8.Cursor = Cursors.Hand;
+            btn_opc4_uc8.Font = new Font("Tahoma", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_opc4_uc8.ForeColor = Color.White;
+            btn_opc4_uc8.Location = new Point(168, 348);
+            btn_opc4_uc8.Margin = new Padding(3, 2, 3, 2);
+            btn_opc4_uc8.Name = "btn_opc4_uc8";
+            btn_opc4_uc8.Size = new Size(150, 150);
+            btn_opc4_uc8.TabIndex = 1;
+            btn_opc4_uc8.Text = "4";
+            btn_opc4_uc8.UseVisualStyleBackColor = false;
+            btn_opc4_uc8.Click += btn_opc4_uc8_Click;
+            // 
+            // btn_opc5_uc8
+            // 
+            btn_opc5_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            btn_opc5_uc8.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_opc5_uc8.Cursor = Cursors.Hand;
+            btn_opc5_uc8.Font = new Font("Tahoma", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_opc5_uc8.ForeColor = Color.White;
+            btn_opc5_uc8.Location = new Point(473, 348);
+            btn_opc5_uc8.Margin = new Padding(3, 2, 3, 2);
+            btn_opc5_uc8.Name = "btn_opc5_uc8";
+            btn_opc5_uc8.Size = new Size(150, 150);
+            btn_opc5_uc8.TabIndex = 1;
+            btn_opc5_uc8.Text = "5";
+            btn_opc5_uc8.UseVisualStyleBackColor = false;
+            btn_opc5_uc8.Click += btn_opc5_uc8_Click;
+            // 
+            // btn_opc6_uc8
+            // 
+            btn_opc6_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            btn_opc6_uc8.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_opc6_uc8.Cursor = Cursors.Hand;
+            btn_opc6_uc8.Font = new Font("Tahoma", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_opc6_uc8.ForeColor = Color.White;
+            btn_opc6_uc8.Location = new Point(776, 348);
+            btn_opc6_uc8.Margin = new Padding(3, 2, 3, 2);
+            btn_opc6_uc8.Name = "btn_opc6_uc8";
+            btn_opc6_uc8.Size = new Size(150, 150);
+            btn_opc6_uc8.TabIndex = 1;
+            btn_opc6_uc8.Text = "6";
+            btn_opc6_uc8.UseVisualStyleBackColor = false;
+            btn_opc6_uc8.Click += btn_opc6_uc8_Click;
+            // 
+            // btn_opc7_uc8
+            // 
+            btn_opc7_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            btn_opc7_uc8.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_opc7_uc8.Cursor = Cursors.Hand;
+            btn_opc7_uc8.Font = new Font("Tahoma", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_opc7_uc8.ForeColor = Color.White;
+            btn_opc7_uc8.Location = new Point(168, 529);
+            btn_opc7_uc8.Margin = new Padding(3, 2, 3, 2);
+            btn_opc7_uc8.Name = "btn_opc7_uc8";
+            btn_opc7_uc8.Size = new Size(150, 150);
+            btn_opc7_uc8.TabIndex = 1;
+            btn_opc7_uc8.Text = "7";
+            btn_opc7_uc8.UseVisualStyleBackColor = false;
+            btn_opc7_uc8.Click += btn_opc7_uc8_Click;
+            // 
+            // btn_opc8_uc8
+            // 
+            btn_opc8_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            btn_opc8_uc8.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_opc8_uc8.Cursor = Cursors.Hand;
+            btn_opc8_uc8.Font = new Font("Tahoma", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_opc8_uc8.ForeColor = Color.White;
+            btn_opc8_uc8.Location = new Point(473, 529);
+            btn_opc8_uc8.Margin = new Padding(3, 2, 3, 2);
+            btn_opc8_uc8.Name = "btn_opc8_uc8";
+            btn_opc8_uc8.Size = new Size(150, 150);
+            btn_opc8_uc8.TabIndex = 1;
+            btn_opc8_uc8.Text = "8";
+            btn_opc8_uc8.UseVisualStyleBackColor = false;
+            btn_opc8_uc8.Click += btn_opc8_uc8_Click;
+            // 
+            // btn_opc9_uc8
+            // 
+            btn_opc9_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            btn_opc9_uc8.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_opc9_uc8.Cursor = Cursors.Hand;
+            btn_opc9_uc8.Font = new Font("Tahoma", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_opc9_uc8.ForeColor = Color.White;
+            btn_opc9_uc8.Location = new Point(776, 529);
+            btn_opc9_uc8.Margin = new Padding(3, 2, 3, 2);
+            btn_opc9_uc8.Name = "btn_opc9_uc8";
+            btn_opc9_uc8.Size = new Size(150, 150);
+            btn_opc9_uc8.TabIndex = 1;
+            btn_opc9_uc8.Text = "9";
+            btn_opc9_uc8.UseVisualStyleBackColor = false;
+            btn_opc9_uc8.Click += btn_opc9_uc8_Click;
+            // 
+            // txt_opc1_uc8
+            // 
+            txt_opc1_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            txt_opc1_uc8.BorderStyle = BorderStyle.None;
+            txt_opc1_uc8.Font = new Font("Tahoma", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txt_opc1_uc8.ForeColor = Color.White;
+            txt_opc1_uc8.Location = new Point(168, 155);
+            txt_opc1_uc8.Multiline = true;
+            txt_opc1_uc8.Name = "txt_opc1_uc8";
+            txt_opc1_uc8.Size = new Size(150, 169);
+            txt_opc1_uc8.TabIndex = 2;
+            txt_opc1_uc8.Text = "Que uma torneira aberta gasta até 12 litros de água por minuto. Por isso, fechar a torneira enquanto escova os dentes ou lava a louça economiza muito.\n";
+            txt_opc1_uc8.TextAlign = HorizontalAlignment.Center;
+            // 
+            // txt_opc2_uc8
+            // 
+            txt_opc2_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            txt_opc2_uc8.BorderStyle = BorderStyle.None;
+            txt_opc2_uc8.Font = new Font("Tahoma", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txt_opc2_uc8.ForeColor = Color.White;
+            txt_opc2_uc8.Location = new Point(473, 155);
+            txt_opc2_uc8.Multiline = true;
+            txt_opc2_uc8.Name = "txt_opc2_uc8";
+            txt_opc2_uc8.Size = new Size(150, 150);
+            txt_opc2_uc8.TabIndex = 2;
+            txt_opc2_uc8.Text = "Que vazamentos pequenos fazem grandes desperdícios! Uma torneira pingando pode desperdiçar até 40 litros por dia.";
+            txt_opc2_uc8.TextAlign = HorizontalAlignment.Center;
+            txt_opc2_uc8.TextChanged += txt_opc2_uc8_TextChanged;
+            // 
+            // txt_opc3_uc8
+            // 
+            txt_opc3_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            txt_opc3_uc8.BorderStyle = BorderStyle.None;
+            txt_opc3_uc8.Font = new Font("Tahoma", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txt_opc3_uc8.ForeColor = Color.White;
+            txt_opc3_uc8.Location = new Point(776, 155);
+            txt_opc3_uc8.Multiline = true;
+            txt_opc3_uc8.Name = "txt_opc3_uc8";
+            txt_opc3_uc8.Size = new Size(150, 150);
+            txt_opc3_uc8.TabIndex = 2;
+            txt_opc3_uc8.Text = "Que lavar o carro com mangueira gasta cerca de 300 litros de água, enquanto um balde e um pano gastam apenas 60 litros.";
+            txt_opc3_uc8.TextAlign = HorizontalAlignment.Center;
+            txt_opc3_uc8.TextChanged += txt_opc3_uc8_TextChanged;
+            // 
+            // txt_opc4_uc8
+            // 
+            txt_opc4_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            txt_opc4_uc8.BorderStyle = BorderStyle.None;
+            txt_opc4_uc8.Font = new Font("Tahoma", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txt_opc4_uc8.ForeColor = Color.White;
+            txt_opc4_uc8.Location = new Point(168, 348);
+            txt_opc4_uc8.Multiline = true;
+            txt_opc4_uc8.Name = "txt_opc4_uc8";
+            txt_opc4_uc8.Size = new Size(150, 150);
+            txt_opc4_uc8.TabIndex = 2;
+            txt_opc4_uc8.Text = "Que fabricar uma calça jeans requer cerca de 7.500 litros de água, desde o cultivo do algodão até a produção final.";
+            txt_opc4_uc8.TextAlign = HorizontalAlignment.Center;
+            txt_opc4_uc8.TextChanged += txt_opc4_uc8_TextChanged;
+            // 
+            // txt_opc5_uc8
+            // 
+            txt_opc5_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            txt_opc5_uc8.BorderStyle = BorderStyle.None;
+            txt_opc5_uc8.Font = new Font("Tahoma", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txt_opc5_uc8.ForeColor = Color.White;
+            txt_opc5_uc8.Location = new Point(473, 369);
+            txt_opc5_uc8.Multiline = true;
+            txt_opc5_uc8.Name = "txt_opc5_uc8";
+            txt_opc5_uc8.Size = new Size(150, 102);
+            txt_opc5_uc8.TabIndex = 2;
+            txt_opc5_uc8.Text = "Que cerca de 2,2 bilhões de pessoas no mundo não têm acesso a água potável segura.";
+            txt_opc5_uc8.TextAlign = HorizontalAlignment.Center;
+            // 
+            // txt_opc6_uc8
+            // 
+            txt_opc6_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            txt_opc6_uc8.BorderStyle = BorderStyle.None;
+            txt_opc6_uc8.Font = new Font("Tahoma", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txt_opc6_uc8.ForeColor = Color.White;
+            txt_opc6_uc8.Location = new Point(776, 334);
+            txt_opc6_uc8.Multiline = true;
+            txt_opc6_uc8.Name = "txt_opc6_uc8";
+            txt_opc6_uc8.Size = new Size(150, 190);
+            txt_opc6_uc8.TabIndex = 2;
+            txt_opc6_uc8.Text = "Que um banho de 10 minutos com chuveiro convencional pode consumir até 150 litros de água. Se o chuveiro for mais econômico, esse valor pode cair para 50 litros.";
+            txt_opc6_uc8.TextAlign = HorizontalAlignment.Center;
+            // 
+            // txt_opc7_uc8
+            // 
+            txt_opc7_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            txt_opc7_uc8.BorderStyle = BorderStyle.None;
+            txt_opc7_uc8.Font = new Font("Tahoma", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txt_opc7_uc8.ForeColor = Color.White;
+            txt_opc7_uc8.Location = new Point(168, 513);
+            txt_opc7_uc8.Multiline = true;
+            txt_opc7_uc8.Name = "txt_opc7_uc8";
+            txt_opc7_uc8.Size = new Size(150, 207);
+            txt_opc7_uc8.TabIndex = 2;
+            txt_opc7_uc8.Text = "Que escovar os dentes com a torneira aberta pode desperdiçar até 6 litros de água. Se você fechar a torneira enquanto escova os dentes, pode economizar muita água no final do dia.";
+            txt_opc7_uc8.TextAlign = HorizontalAlignment.Center;
+            txt_opc7_uc8.TextChanged += txt_opc7_uc8_TextChanged;
+            // 
+            // txt_opc8_uc8
+            // 
+            txt_opc8_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            txt_opc8_uc8.BorderStyle = BorderStyle.None;
+            txt_opc8_uc8.Font = new Font("Tahoma", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txt_opc8_uc8.ForeColor = Color.White;
+            txt_opc8_uc8.Location = new Point(776, 538);
+            txt_opc8_uc8.Multiline = true;
+            txt_opc8_uc8.Name = "txt_opc8_uc8";
+            txt_opc8_uc8.Size = new Size(150, 141);
+            txt_opc8_uc8.TabIndex = 2;
+            txt_opc8_uc8.Text = "Que lavar louçapode gastar até 5 litros de água por prato, dependendo do tamanho e do tempo que a torneira fica aberta.";
+            txt_opc8_uc8.TextAlign = HorizontalAlignment.Center;
+            // 
+            // txt_opc9_uc8
+            // 
+            txt_opc9_uc8.BackColor = Color.FromArgb(0, 14, 39);
+            txt_opc9_uc8.BorderStyle = BorderStyle.None;
+            txt_opc9_uc8.Font = new Font("Tahoma", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txt_opc9_uc8.ForeColor = Color.White;
+            txt_opc9_uc8.Location = new Point(473, 529);
+            txt_opc9_uc8.Multiline = true;
+            txt_opc9_uc8.Name = "txt_opc9_uc8";
+            txt_opc9_uc8.Size = new Size(150, 188);
+            txt_opc9_uc8.TabIndex = 2;
+            txt_opc9_uc8.Text = "Que usar uma mangueira para limpar as janelas pode consumir até 50 litros de água por vez. Usando um pano úmido e balde, você reduz esse número drasticamente.";
+            txt_opc9_uc8.TextAlign = HorizontalAlignment.Center;
             // 
             // ucPage2
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(0, 14, 39);
-            Controls.Add(textBox1);
+            Controls.Add(txt_opc9_uc8);
+            Controls.Add(txt_opc8_uc8);
+            Controls.Add(txt_opc7_uc8);
+            Controls.Add(txt_opc6_uc8);
+            Controls.Add(txt_opc5_uc8);
+            Controls.Add(txt_opc4_uc8);
+            Controls.Add(txt_opc3_uc8);
+            Controls.Add(txt_opc2_uc8);
+            Controls.Add(txt_opc1_uc8);
+            Controls.Add(btn_opc9_uc8);
+            Controls.Add(btn_opc6_uc8);
+            Controls.Add(btn_opc3_uc8);
+            Controls.Add(btn_opc8_uc8);
+            Controls.Add(btn_opc5_uc8);
+            Controls.Add(btn_opc2_uc8);
+            Controls.Add(btn_opc7_uc8);
+            Controls.Add(btn_opc4_uc8);
+            Controls.Add(btn_opc1_uc8);
             Controls.Add(label1);
             Name = "ucPage2";
             Size = new Size(1080, 720);
@@ -74,6 +368,23 @@
         #endregion
 
         private Label label1;
-        private TextBox textBox1;
+        private Button btn_opc1_uc8;
+        private Button btn_opc2_uc8;
+        private Button btn_opc3_uc8;
+        private Button btn_opc4_uc8;
+        private Button btn_opc5_uc8;
+        private Button btn_opc6_uc8;
+        private Button btn_opc7_uc8;
+        private Button btn_opc8_uc8;
+        private Button btn_opc9_uc8;
+        private TextBox txt_opc1_uc8;
+        private TextBox txt_opc2_uc8;
+        private TextBox txt_opc3_uc8;
+        private TextBox txt_opc4_uc8;
+        private TextBox txt_opc5_uc8;
+        private TextBox txt_opc6_uc8;
+        private TextBox txt_opc7_uc8;
+        private TextBox txt_opc8_uc8;
+        private TextBox txt_opc9_uc8;
     }
 }
--- UserControl8.cs
+++ UserControl8.cs
@@ -15,11 +15,100 @@ namespace waterApp
         public ucPage2()
         {
             InitializeComponent();
+            txt_opc1_uc8.Hide();
+            txt_opc2_uc8.Hide();
+            txt_opc3_uc8.Hide();
+            txt_opc4_uc8.Hide();
+            txt_opc5_uc8.Hide();
+            txt_opc6_uc8.Hide();
+            txt_opc7_uc8.Hide();
+            txt_opc8_uc8.Hide();
+            txt_opc9_uc8.Hide();
+
+            if (txt_opc1_uc8.Visible && txt_opc2_uc8.Visible && txt_opc3_uc8.Visible &&
+    txt_opc4_uc8.Visible && txt_opc5_uc8.Visible && txt_opc6_uc8.Visible &&
+    txt_opc7_uc8.Visible && txt_opc8_uc8.Visible && txt_opc9_uc8.Visible)
+            {
+               
+            }
+
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void btndica1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_opc1_uc8_Click(object sender, EventArgs e)
+        {
+            txt_opc1_uc8.Show();
+        }
+
+        private void txt_opc2_uc8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_opc2_uc8_Click(object sender, EventArgs e)
+        {
+            txt_opc2_uc8.Show();
+        }
+
+        private void txt_opc3_uc8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_opc3_uc8_Click(object sender, EventArgs e)
+        {
+            txt_opc3_uc8.Show();
+        }
+
+        private void btn_opc4_uc8_Click(object sender, EventArgs e)
+        {
+            txt_opc4_uc8.Show();
+        }
+
+        private void txt_opc4_uc8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_opc7_uc8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_opc5_uc8_Click(object sender, EventArgs e)
+        {
+            txt_opc5_uc8.Show();
+        }
+
+        private void btn_opc6_uc8_Click(object sender, EventArgs e)
+        {
+            txt_opc6_uc8.Show();
+        }
+
+        private void btn_opc7_uc8_Click(object sender, EventArgs e)
+        {
+            txt_opc7_uc8.Show();
+        }
+
+        private void btn_opc8_uc8_Click(object sender, EventArgs e)
+        {
+            txt_opc8_uc8.Show();
+        }
+
+        private void btn_opc9_uc8_Click(object sender, EventArgs e)
+        {
+            txt_opc9_uc8.Show();
+        }
+
+        
     }
 }
